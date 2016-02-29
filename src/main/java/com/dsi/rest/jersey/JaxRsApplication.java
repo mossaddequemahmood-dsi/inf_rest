c7e9b31af25a4b1050677c0d7fe5624f7424fea6
Mossaddeque Mahmood <mossaddequemahmood.dsi@gmail.com>
2016-02-29 18:10:57 +0600

    Issue #10, make app compitable for tomcat, create example project.

--- src/main/java/com/dsi/rest/jersey/JaxRsApplication.java
+++ src/main/java/com/dsi/rest/jersey/JaxRsApplication.java
@@ -1,9 +1,12 @@
 package com.dsi.rest.jersey;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.dsi.rest.Bootstrap;
 
+@ApplicationPath("")
 public class JaxRsApplication extends ResourceConfig {
 
 	public JaxRsApplication() {
